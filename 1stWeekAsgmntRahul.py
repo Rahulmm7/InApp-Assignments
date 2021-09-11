d6e5e3d5339d5a7efd553a303ba705915e11fed5
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2021-09-12 01:40:33 +0530

    Update 1stWeekAsgmntRahul.py

--- 1stWeekAsgmntRahul.py
+++ 1stWeekAsgmntRahul.py
@@ -1,4 +1,3 @@
-
 import random
 
 print("Rules of the game as follows: \n"
@@ -9,6 +8,25 @@ count = 0
 user = 0
 comp = 0
 dicts = {}
+
+def rules(a,b):
+    a = choice
+    b = comp_choice
+    if ((choice == 1 and comp_choice == 2) or
+            (choice == 2 and comp_choice == 1)):
+        print("paper wins => ", end="")
+        result = "paper"
+
+    elif ((choice == 1 and comp_choice == 3) or
+          (choice == 3 and comp_choice == 1)):
+        print("Rock wins =>", end="")
+        result = "Rock"
+    else:
+        print("scissor wins =>", end="")
+        result = "scissor"
+
+    return result
+
 while count < 10 :
 
     print("Enter choice \n 1. Rock \n 2. paper \n 3. scissor \n")
@@ -28,7 +46,7 @@ while count < 10 :
         choice_name = 'scissor'
 
 
-    print("user choice is: " + choice_name)
+    print(f"user choice is: {choice_name} " )
     print("\nNow its computer turn.......")
 
 
@@ -46,51 +64,29 @@ while count < 10 :
     else:
         comp_choice_name = 'scissor'
 
-    print("Computer choice is: " + comp_choice_name)
+    print(f"Computer choice is: {comp_choice_name} ")
 
     print(choice_name + " V/s " + comp_choice_name)
 
 
-    if ((choice == 1 and comp_choice == 2) or
-            (choice == 2 and comp_choice == 1)):
-        print("paper wins => ", end="")
-        result = "paper"
-
-    elif ((choice == 1 and comp_choice == 3) or
-          (choice == 3 and comp_choice == 1)):
-        print("Rock wins =>", end="")
-        result = "Rock"
-    else:
-        print("scissor wins =>", end="")
-        result = "scissor"
-
+    result = rules(choice,comp_choice)
 
     if result == choice_name:
         print(" User wins ")
         a = "user"
-        user = user + 1
+        user += 1
     else:
         print(" Computer wins ")
         a = "computer"
-        comp = comp + 1
+        comp +=  1
 
     dicts[0 + count] = [choice_name, comp_choice_name, a]
-    print(dicts)
-
-
-
-
-
-
+    #print(dicts)
 
     count = count + 1
 
 
-
-print("\n")
-print("Points by user : " + str(user))
-print("Points by computer : " + str(comp))
-
+print(f"\nPoints by user : {user} \nPoints by computer : {comp}" )
 
 if user > comp:
     print("User wins overall")
@@ -99,9 +95,19 @@ elif comp > user:
     print("computer wins overall ")
 
 else:
-    print("game ended TIE")
+    print("Game ended TIE")
 
-m = int(input("Enter the round for which you need the information :"))
-print(dicts[m-1])
+f='y'
+while(f=='y'):
+    try :
+        m = int(input("Enter the round for which you need the information :"))
+
+        print(f"Player Choice is {dicts[m-1][0]}" )
+        print(f"Computer Choice is {dicts[m-1][1]}")
+        print(f"{dicts[m-1][2]} has won round {m}", )
+    except:
+        print("The round number doesn't exist")
+    f = input("Do you want to continue (y/n)")
+
 
 print("\nThanks for playing")
