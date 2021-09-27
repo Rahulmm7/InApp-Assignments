2ea71e06bca6c2cc6354801233f3905b24be0f76
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2021-09-27 19:11:35 +0530

    Update Assignment1Qsnt1.py

--- Database/Assignment1Qsnt1.py
+++ Database/Assignment1Qsnt1.py
@@ -1,26 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect('cars.db')
+conn = sqlite3.connect(':memory:')
 cursor = conn.cursor()
 
-
-querry = """ create TABLE Cars(
+querry = """ CREATE TABLE Cars(
                                 Car_name char(20) NOT NULL,
                                 Car_Owner CHAR(20) NOT NULL)"""
 cursor.execute(querry)
 
+manyCars = [
+            ('ford','henry'),
+            ('nisaan','majid'),
+            ('maruthi','sabu'),
+            ('Thar','amal'),
+            ('BMW','henry'),
+            ('ferrari','nunu'),
+            ('KIA','akash'),
+            ('innova','sasi'),
+            ('cruze','pradeep'),
+            ('polo GT','vyshak')
+                        ]
 
-cursor.execute("insert into Cars values('ford','henry')")
-cursor.execute("insert into Cars values('nisaan','majid')")
-cursor.execute("insert into Cars values('maruthi','sabu')")
-cursor.execute("insert into Cars values('Thar', 'amal')")
-cursor.execute("insert into Cars values('BMW','nunu')")
-cursor.execute("insert into Cars values('ferrari','enzo')")
-cursor.execute("insert into Cars values('KIA','sasi')")
-cursor.execute("insert into Cars values('innova','akash')")
-cursor.execute("insert into Cars values('cruze','pradeep')")
-cursor.execute("insert into Cars values('polo GT','vyshak')")
-
+cursor.executemany("INSERT INTO Cars VALUES (?,?) ",manyCars)
 
 cursor.execute(" select * from Cars ")
 print(cursor.fetchall())
